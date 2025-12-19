832fce8f538989756cfb810b5fde94ba5f7c8561
dianfitrip <dianfitri683@gmail.com>
2025-12-19 20:18:43 +0700

    menambahkan container aplikasi dan konfigurasi Retrofit untuk repository siswa

---
+++ app/src/main/java/com/example/myapplication/viewmodel/EntryVIewModel.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.viewmodel
+
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.myapplication.modeldata.DetailSiswa
+import com.example.myapplication.modeldata.UIStateSiswa
+import com.example.myapplication.modeldata.toDataSiswa
+import com.example.myapplication.repository.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel (private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
+            Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    //Fungsi untuk menangani saat ada perubahan pada text input
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(detailSiswa, validasiInput(detailSiswa))
+    }
+
+
+}
