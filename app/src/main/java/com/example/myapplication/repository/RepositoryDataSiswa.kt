4fd4e340b54b76726a7676d91f282845582116ab
dianfitrip <dianfitri683@gmail.com>
2025-12-19 10:45:49 +0700

    menambahkan RepositoryDataSiswa dan implementasi JaringanRepositoryDataSiswa untuk akses API siswa

---
+++ app/src/main/java/com/example/myapplication/repository/RepositoryDataSiswa.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.repository
+
+
+import com.example.myapplication.apiservice.ServiceApiSiswa
+import com.example.myapplication.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa {
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+}
