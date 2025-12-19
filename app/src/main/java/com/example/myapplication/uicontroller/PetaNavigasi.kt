b664aa0d2ea151aad6f840ff5ab1af161aaaa320
dianfitrip <dianfitri683@gmail.com>
2025-12-19 20:50:57 +0700

    menambahkan container aplikasi dan konfigurasi Retrofit untuk repository siswa

---
+++ app/src/main/java/com/example/myapplication/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication.uicontroller
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.uicontroller.route.DestinasiEntry
+import com.example.myapplication.uicontroller.route.DestinasiHome
+import com.example.myapplication.view.EntrySiswaScreen
+import com.example.myapplication.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },)
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome
+                .route) })
+        }
+    }
+}
