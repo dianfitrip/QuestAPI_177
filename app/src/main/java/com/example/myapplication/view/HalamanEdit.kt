392404c84c74738155526b1b9fa7da1b2b7fcee0
dianfitrip <dianfitri683@gmail.com>
2025-12-26 16:39:49 +0700

    menambahkan import dihalaman edit

--- app/src/main/java/com/example/myapplication/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myapplication/view/HalamanEdit.kt
@@ -1,5 +1,17 @@
 package com.example.myapplication.view
 
+import com.example.myapplication.viewmodel.provider.PenyediaViewModel
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapplication.uicontroller.route.DestinasiEdit
+import com.example.myapplication.viewmodel.EditViewModel
+import kotlinx.coroutines.launch
 
 
 
