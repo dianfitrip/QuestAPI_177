c63df7c7e17baab767b4f670d3c4921e56799c6e
dianfitrip <dianfitri683@gmail.com>
2025-12-26 14:40:42 +0700

    menambahkan fungsi get, edit, dan hapus satu data siswa

--- app/src/main/java/com/example/myapplication/repository/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/myapplication/repository/RepositoryDataSiswa.kt
@@ -7,6 +7,10 @@ import com.example.myapplication.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    suspend fun getSatuSiswa(id: Int) : DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int) : retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
