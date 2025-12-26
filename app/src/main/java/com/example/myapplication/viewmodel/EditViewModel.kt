61ff8dcf41c10ed32cf20fbd6179c6df14fe93a0
dianfitrip <dianfitri683@gmail.com>
2025-12-26 16:50:01 +0700

    menambahkan EditViewModel untuk memuat dan mengelola data siswa yang akan diedit

---
+++ app/src/main/java/com/example/myapplication/viewmodel/EditViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication.viewmodel
+
+
+
+
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            try {
+                val dataSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                uiStateSiswa = dataSiswa.toUiStateSiswa(true)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                println("Error saat ambil data siswa: ${e.message}")
+            }
+        }
+    }
+}
