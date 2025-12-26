599dbb098f46b59d06f8e12042415db63c9e7bb2
dianfitrip <dianfitri683@gmail.com>
2025-12-26 15:43:31 +0700

    menambahkan navigasi ke halaman edit siswa dengan parameter ID

--- app/src/main/java/com/example/myapplication/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/myapplication/uicontroller/PetaNavigasi.kt
@@ -43,5 +43,11 @@ fun HostNavigasi(
                 navigateBack = { navController.navigate(DestinasiHome.route) }
             )
         }
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.IntType
+            })
+        ){}
     }
 }
